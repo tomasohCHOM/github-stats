ea72c896530646662a5dc2866bd383989b8bf71c
Tomas Oh <tomasoh@csu.fullerton.edu>
2024-05-31 10:59:34 -0700

    Set up envs and github client

--- cmd/main.go
+++ cmd/main.go
@@ -10,34 +10,91 @@ import (
 	"os"
 
 	"github.com/google/go-github/github"
+	"github.com/joho/godotenv"
 	"github.com/urfave/cli/v2"
+	"golang.org/x/oauth2"
 )
 
 func main() {
-	app := &cli.App{
-		Name:  "github-stats",
-		Usage: "Generate GitHub stats from a user",
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:     "username",
-				Usage:    "Match the github username",
-				Required: true,
-			},
-		},
-		Action: action,
+	ctx := context.Background()
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Unable to load .env file")
 	}
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
+	token := os.Getenv("ACCESS_TOKEN")
+	if token == "" {
+		log.Fatal("GITHUB_TOKEN environment variable not set")
 	}
+
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+	owner := "EthanThatOneKid" // Replace with the repository owner
+	repo := "acmcsuf.com"      // Replace with the repository name
+
+	// Fetch open issues count
+	repoStats, _, err := client.Repositories.Get(ctx, owner, repo)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	openIssuesCount := 0
+	if *repoStats.HasIssues {
+		openIssuesCount = repoStats.GetOpenIssuesCount()
+	}
+
+	fmt.Printf("Open Issues: %d\n", openIssuesCount)
+
+	// app := &cli.App{
+	// Name:  "github-stats",
+	// Usage: "Generate GitHub stats from a user/organization",
+	//:! Flags: []cli.Flag{
+	// &cli.StringFlag{
+	// Name:     "username",
+	// Usage:    "Match the github username",
+	// Required: true,
+	// },
+	// },
+	// Action: action,
+	// }
+	// if err := app.Run(os.Args); err != nil {
+	// log.Fatal(err)
+	// }
 }
 
 func action(ctx *cli.Context) error {
 	username := ctx.String("username")
 	client := github.NewClient(nil)
-	response, _, err := client.Repositories.List(context.Background(), username, nil)
-	if err != nil {
-		log.Fatal(err)
+	opt := &github.RepositoryListOptions{ListOptions: github.ListOptions{PerPage: 10}}
+
+	var allRepos []*github.Repository
+	totalPRs := 0
+
+	for {
+		repos, resp, err := client.Repositories.List(context.Background(), username, opt)
+		if err != nil {
+			log.Fatal(err)
+		}
+		allRepos = append(allRepos, repos...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
 	}
-	fmt.Println(response)
+
+	for _, repository := range allRepos {
+		prs, _, err := client.PullRequests.List(context.Background(), username, repository.GetName(), &github.PullRequestListOptions{State: "open, closed"})
+		if err != nil {
+			log.Fatal(err)
+		}
+		totalPRs += len(prs)
+
+		fmt.Println(repository.GetName())
+		fmt.Println("Total Pull Requests: ", len(prs))
+	}
+
 	return nil
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,8 @@ require (
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
+	golang.org/x/oauth2 v0.20.0 // indirect
 )
